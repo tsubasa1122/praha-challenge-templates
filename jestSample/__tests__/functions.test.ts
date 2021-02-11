53215a2ab95df9d90c26a89d45d54d184161aa4b
tsubasa1122 <nakanot@open8.com>
2021-02-11 19:34:04 +0900

    Add tests sumOfArray and asyncSumOfArray

--- jestSample/__tests__/functions.test.ts
+++ jestSample/__tests__/functions.test.ts
@@ -1,1 +1,37 @@
 // todo: ここに単体テストを書いてみましょう！
+import { sumOfArray, asyncSumOfArray } from '../functions';
+
+describe('sumOfArray', () => {
+  describe('numberの配列を渡した時', () => {
+    it('numberの合計値を返すこと', () => {
+      expect(sumOfArray([1, 2])).toBe(3);
+      expect(sumOfArray([1, 2, 3])).toBe(6);
+    });
+  });
+
+  describe('空の配列を渡した時', () => {
+    it('例外が発生すること', () => {
+      expect(() => sumOfArray([])).toThrow(TypeError);
+      expect(() => sumOfArray([])).toThrow(
+        'Reduce of empty array with no initial value'
+      );
+    });
+  });
+});
+
+describe('asyncSumOfArray', () => {
+  describe('numberの配列を渡した時', () => {
+    it('numberの合計値を返すこと', () => {
+      return expect(asyncSumOfArray([1, 2])).resolves.toBe(3);
+    });
+  });
+
+  describe('空の配列を渡した時', () => {
+    it('例外が発生すること', () => {
+      expect(asyncSumOfArray([])).rejects.toThrow(TypeError);
+      expect(asyncSumOfArray([])).rejects.toThrow(
+        'Reduce of empty array with no initial value'
+      );
+    });
+  });
+});
