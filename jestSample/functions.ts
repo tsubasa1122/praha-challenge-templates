a23b4f79fe9ca5279f174f56b9e23c6fd7b4c5e4
tsubasa1122 <nakanot@open8.com>
2021-02-13 19:18:28 +0900

    Answer to jestで単体テストを書こう question2

--- jestSample/functions.ts
+++ jestSample/functions.ts
@@ -1,5 +1,5 @@
-import { NameApiService } from "./nameApiService";
-import { DatabaseMock } from "./util";
+import { NameApiService } from './nameApiService';
+import { DatabaseMock } from './util';
 
 export const sumOfArray = (numbers: number[]): number => {
   return numbers.reduce((a: number, b: number): number => a + b);
@@ -12,11 +12,11 @@ export const asyncSumOfArray = (numbers: number[]): Promise<number> => {
 };
 
 export const asyncSumOfArraySometimesZero = (
-  numbers: number[]
+  numbers: number[],
+  database: DatabaseMock
 ): Promise<number> => {
   return new Promise((resolve): void => {
     try {
-      const database = new DatabaseMock(); // fixme: この関数をテストするには、DatabaseMockの使い方を変える必要がありそう！ヒント：依存性の注入
       database.save(numbers);
       resolve(sumOfArray(numbers));
     } catch (error) {
@@ -26,13 +26,13 @@ export const asyncSumOfArraySometimesZero = (
 };
 
 export const getFirstNameThrowIfLong = async (
-  maxNameLength: number
+  maxNameLength: number,
+  nameApiSerivce: NameApiService
 ): Promise<string> => {
-  const nameApiSerivce = new NameApiService(); // fixme: この関数をテストするには、NameApiServiceの使い方を変える必要がありそう！ヒント：依存性の注入
   const firstName = await nameApiSerivce.getFirstName();
 
   if (firstName.length > maxNameLength) {
-    throw new Error("first_name too long");
+    throw new Error('first_name too long');
   }
   return firstName;
 };
